e12124926b2fdb0d9f9c341f681b9e908561a90d
vubui-dev <vubuidev@gmail.com>
2020-04-18 05:46:29 +0700

    complete path 17/4/2020

--- src/helpers/token.js
+++ src/helpers/token.js
@@ -1,2 +1,4 @@
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTZjOTM4NGFiYmZjNDQ4NThiMTdkZWEiLCJtTmFtZSI6IlPhu69hIFPhu69hIiwibUVtYWlsIjoic3Vhc3VhQGdtYWlsLmNvbSIsIm1BZ2UiOm51bGwsIm1Sb2xlIjpudWxsLCJtSXNBZG1pbiI6ZmFsc2UsImZJRCI6IjVlNmI3YWFlNjUyYjAzM2IxYzkwZTA3ZiIsImlhdCI6MTU4NDE3Njg5M30.XJBgpNMD2zubJFyTTWF3qm-99h4DFPmlP53pQRZrj-k';
+const inforLogin = JSON.parse(localStorage.getItem("inforLogin")) || null;
+const { token } = inforLogin || null;
+
 export default token;
